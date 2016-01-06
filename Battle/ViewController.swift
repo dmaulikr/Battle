d1c6bebf76a1a00a4a553268fd60abd60020c7e0
Jonny B <jonnyb@Jonnys-MacBook-Pro.local>
2016-01-06 00:25:54 -0700

    Added storyboard elements and started adding some outlets

--- Battle/ViewController.swift
+++ Battle/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var grasslandsBtn: UIButton!
+    @IBOutlet weak var dungeonBtn: UIButton!
+    
+    @IBOutlet weak var bg: UIImageView!
+    @IBOutlet weak var groundImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func grasslandsBtnPressed(sender: UIButton) {
+        
+        bg.image = UIImage(named: "bg_battle")
+        groundImg.image = UIImage(named: "ground_battle")
+     
+    }
 
+    @IBAction func dungeonBtnPressed(sender: UIButton) {
+        
+        bg.image = UIImage(named: "bg")
+        groundImg.image = UIImage(named: "ground")
+    }
 }
 
